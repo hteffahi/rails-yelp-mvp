d024691493370003a8817df7ae54d49c3465f973
houssem <houssem.teffahi@gmail.com>
2023-11-17 11:21:11 +0100

    rails-yelp-mvp

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "3.1.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.8"
 
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
@@ -41,6 +42,8 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+
+gem "simple_form"
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Review.destroy_all
+Restaurant.destroy_all
+
+# Create restaurants
+restaurants_data = [
+  { name: "Chez Pierre", address: "123 Main St", phone_number: "555-1234", category: "french" },
+  { name: "Sushi Palace", address: "456 Oak St", phone_number: "555-5678", category: "japanese" },
+  { name: "Mamma Mia", address: "789 Elm St", phone_number: "555-9876", category: "italian" },
+  { name: "Wok Express", address: "101 Pine St", phone_number: "555-5432", category: "chinese" },
+  { name: "Belgian Bites", address: "202 Cedar St", phone_number: "555-8765", category: "belgian" }
+]
+
+restaurants_data.each do |restaurant_data|
+  Restaurant.create!(restaurant_data)
+end
+
+puts 'Seed completed successfully!'
